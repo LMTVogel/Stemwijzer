d5bdb7787589bf68b308c0d3dd71f88ea16a9231
Luuk <99051050@mydavinci.nl>
2020-04-28 12:00:10 +0200

    De code slaat de vragen op.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var vraagCounter = 0;
 
+subjects.forEach(subject => {
+    subject.myAnswer = '';
+});
+
+parties.forEach(party => {
+    party.points = 0;
+});
+
+
 function startKnop() {
     document.getElementById('homePage').style.display='none';
     document.getElementById('stellingPage').style.display='block';
@@ -12,4 +21,42 @@ function displayVraag() {
 
     document.getElementById('stellingTitle').innerHTML=(vraagNummer+1).toString() + '. ' + subjects[vraagCounter].title;
     document.getElementById('stellingDes').innerHTML=subjects[vraagCounter].statement;
+}
+
+function nextStatement() {
+    if(vraagCounter == subjects.length-1) {
+
+        calculatePoints();
+
+    }else {
+        vraagCounter++;
+        console.log(subjects[vraagCounter]);
+        displayVraag();
+    }
+}
+
+function previousStatement() {
+    if(vraagCounter == 0) {
+        document.getElementById('homePage').style.display='block';
+        document.getElementById('stellingPage').style.display='none';
+    }else {
+        vraagCounter--;
+        displayVraag();
+    }
+}
+
+function saveAnswer(answer) {
+    subjects[vraagCounter].myAnswer=answer;
+    nextStatement();
+}
+
+function calculatePoints() {
+    subjects.forEach(subject => {
+        subject.parties.forEach(function(party, partyIndex){
+             if(subject.myAnswer == subject.parties[partyIndex].position){
+                var scoreParty = parties.find(party => party.name == subject.parties[partyIndex].name);
+                scoreParty.points += 1;
+            }
+        })
+    })
 }
